8b0195e9caf101ed76a1db368a8222fbcd5dabcf
PiPower <gyros.pl@gmail.com>
2024-02-05 01:24:36 +0100

    error messages added

--- src/Expressions/Addition.cpp
+++ src/Expressions/Addition.cpp
@@ -1,10 +1,22 @@
 #include "Addition.hpp"
+#include <iostream>
+using namespace std;
 
 Addition::Addition(Expression *left_side, Expression *right_side)
 :
 Expression()
 {
-    children.push_back(left_node);
+    if(left_side == nullptr)
+    {
+        cout << "ERROR: left child of node [Addition] cannot be nullptr \n";
+        exit(-1); 
+    }
+    if(right_side == nullptr)
+    {
+        cout << "ERROR: right child of node [Addition] cannot be nullptr \n";
+        exit(-1); 
+    }
+    children.push_back(left_side);
     children.push_back(right_side);
 }
 
--- src/Expressions/Addition.hpp
+++ src/Expressions/Addition.hpp
@@ -8,9 +8,6 @@ class Addition : public Expression
 public:
     Addition( Expression* left_side, Expression* right_side);
     void compile();
-private:
-    Expression* left_node;
-    Expression* right_node;
 };
 
 #endif
